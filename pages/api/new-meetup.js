b1350ed38eb65ec194a3e38e83c980fd8c470666
Lucas  de Oliveira Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-07-30 19:22:16 -0300

    Working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,22 @@
 // POST - api/new-meetup
-function handler(req, res) {
-	if (req.method === 'POST') {
-		const data = req.body;
-		const { title, image, address, description } = data;
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://lucasneves:PMFtmHxjb19MYRLk@sandbox.fflys.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
 
-		
-	}
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({
+      message: "Meetup inserted!",
+    });
+  }
 }
+
+export default handler;
